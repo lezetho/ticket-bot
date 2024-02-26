380d5c29c203eebef26baeabc3343c88aa508606
Lezetho <126505858+lezetho@users.noreply.github.com>
2024-02-26 15:46:53 -0500

    Create ticketSetup.js

---
+++ command/ticketSetup.js
@@ -0,0 +1,39 @@
+const {MessageActionRow, MessageSelectMenu} = require('discord.js')
+const { serverName } = require('../config.json');
+module.exports = {
+    name: 'ticket',
+    usage: 'template',
+    category: "mod",
+    description: `Tickets.`,
+    async execute(client, message, args) {
+		message.delete()
+        const row = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('select')
+					.setPlaceholder('Select the type of ticket to create.')
+					.addOptions([
+						{
+							label: '🤝 | Partner',
+							description: 'Open a partnership ticket.',
+							value: 'partnership',
+						},
+						{
+							label: '📛 | Report',
+							description: 'Open a report ticket ',
+							value: 'report',
+						},
+					]),
+			);
+
+        message.channel.send({
+            embeds: [{
+                title: 'Open ticket',
+                description: '**__How To Open A Ticket :__**\nPlease choose the type of ticket you wish to open.',
+                color: "BLURPLE",
+                footer: {text: `${serverName} | Tickets`}
+            }],
+            components: [row]
+        })
+    }
+}
